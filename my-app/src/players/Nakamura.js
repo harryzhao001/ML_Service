1e759e7943f4352d36770f32f602dbe5c01fbe28
Harry Zhao <h323@uwaterloo.ca>
2020-09-30 22:39:44 -0400

    started react app

---
+++ my-app/src/players/Nakamura.js
@@ -0,0 +1,33 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function Nakamura() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <p>
+                    What type of chess player are you? Find out now!
+                </p>
+                <form>
+                    <label>
+                        Enter your lichess username:
+                        <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    See list of playstyle types
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Nakamura;
