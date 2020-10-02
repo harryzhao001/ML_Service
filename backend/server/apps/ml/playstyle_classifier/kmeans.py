e099f708d80aa431979be35ea9b547c8456c7cb5
Harry Zhao <h323@uwaterloo.ca>
2020-10-02 18:48:37 -0400

    updated models

---
+++ backend/server/apps/ml/playstyle_classifier/kmeans.py
@@ -0,0 +1,33 @@
+import joblib
+import pandas as pd
+
+class KMeansClassifer:
+    def __init__(self):
+        path = "../../research/"
+        self.encoders = joblib.load(path + "encoders-chess.joblib")
+        self.model = joblib.load(path + "kmeans.joblib")
+    
+    def preprocessing(self, input_data):
+        input_data = pd.DataFrame(input_data, index=[0])
+        for column in [
+            'rated', 'victory_status', 'winner', 'increment_code', 'opening_eco'
+        ]:
+            categorical_convert = self.encoders[column]
+            input_data[column] = categorical_convert.transform(input_data[column])
+        
+        return input_data
+
+    def predict(self, input_data):
+        return self.model.fit_predict(input_data)[0]
+
+    def postprocessing(self, input_data):
+        return input_data[0]
+
+    def compute_prediction(self, input_data):
+        try:
+            input_data = self.preprocessing(input_data)
+            prediction = self.predict(input_data)
+        except Exception as e:
+            return {"status": "Error", "message": str(e)}
+
+        return prediction
